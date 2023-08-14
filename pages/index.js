90e86d1c3a48b09db9a3aea338fa12c0aa69a19b
Lucas Santos <lucasprojetos.dev@gmail.com>
2023-08-14 11:20:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Asa de urubu, pena de galinha, se você me ama, da uma risadinha 🤣</h1>
+  return (
+    <h1>Asa de urubu, pena de galinha, se você me ama, da uma risadinha 🤣</h1>
+  );
 }
 
 export default Home;
