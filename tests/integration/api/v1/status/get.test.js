563bf8e7c96715542f6683ab2824ea4991d4ed55
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-04-09 15:39:01 -0300

    correction in tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,19 +9,11 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.version).toEqual("16.0");
 
   expect(responseBody.dependencies.database.max_connections).toBeDefined();
-  expect(responseBody.dependencies.database.max_connections).toBe(1);
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
 
   expect(responseBody.dependencies.database.opened_connections).toBeDefined();
-  expect(typeof responseBody.dependencies.database.opened_connections).toBe(
-    "number",
-  );
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
-  console.log(responseBody);
-});
-
-test.only("Teste de SQL Injection", async () => {
-  await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
 });
