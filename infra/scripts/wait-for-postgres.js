eb1022e7c61315c0a37a461b70cf97c6c47dca48
Lucas Santos <lucasfavaretosantos98@gmail.com>
2024-05-28 18:00:29 -0300

    adds `wait-for-postgres.js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stout) {
+    if (stout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n\n🟢 Postgres está pronto e aceitando conexões");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando postgres aceitar conexões");
+
+checkPostgres();
